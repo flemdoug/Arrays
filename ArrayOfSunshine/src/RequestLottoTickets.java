8b8bc9fe727797b50e19e9497ca2bb3b56e7dee9
Brandon <bekirk0321@gmail.com>
2017-03-30 10:12:40 -0600

    Test commit of Arrays file

---
+++ ArrayOfSunshine/src/RequestLottoTickets.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class RequestLottoTickets {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Test edit for egit");
+	}
+}
